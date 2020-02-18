95210564c7b2d14f70a03e53dd81021abebe9520
DESKTOP-DNIREL5\hiko <a2c2c123@126.com>
2020-02-18 21:49:37 +0800

    add problem5

---
+++ MuyLeetCodeSolu/Medium/Problem5.h
@@ -0,0 +1,105 @@
+#pragma once
+
+/*
+5. Longest Palindromic Substring
+https://leetcode.com/problems/longest-palindromic-substring/
+*/
+
+#include <iostream>
+
+class Problem5
+{
+public:
+
+	static std::string longestPalindrome(std::string inputStr)
+	{
+		if (2 > inputStr.length())
+			return inputStr;
+		else if (inputStr.length() == 2)
+		{
+			if (inputStr[0] != inputStr[1])
+				return inputStr.substr(0, 1);
+			else
+				return inputStr;
+		}
+
+		std::string result = "";
+		int maxlen = 0, strLen = inputStr.length(), left = 0, right = 0;
+		int i = 0, j = 0;
+
+		// check 'abba'
+		for (i = 0; i < strLen - 1; i++)
+		{
+			left = i;
+			right = i + 1;
+			j = 0;
+			while (true)
+			{
+				if (inputStr[left - j] == inputStr[right + j])
+				{
+					j++;
+					if (left - j < 0 || right + j == strLen)
+					{
+						j--;
+						break;
+					}
+				}
+				else
+				{
+					if (j > 0)
+						j--;
+					else
+						j = -1;
+					break;
+				}
+			}
+
+			if (j >= 0 && maxlen < 2 * (j + 1)) // length is 2*(j+1)
+			{
+				// seems like we find a str
+				result = inputStr.substr(left - j, 2 * (j + 1));
+				maxlen = result.length();
+			}
+		}
+
+		// check 'ab?ba'
+		for (i = 1; i < strLen - 1; i++)
+		{
+			left = i - 1;
+			right = i + 1;
+			j = 0;
+			while (true)
+			{
+				if (inputStr[left - j] == inputStr[right + j])
+				{
+					j++;
+					if (left - j < 0 || right + j == strLen)
+					{
+						j--;
+						break;
+					}
+				}
+				else
+				{
+					if (j > 0)
+						j--;
+					else
+						j = -1;
+					break;
+				}
+			}
+
+			if (j >= 0 && maxlen < 2 * (j + 1) + 1)
+			{
+				result = inputStr.substr(left - j, 2 * (j + 1) + 1);
+				maxlen = result.length();
+			}
+
+		}
+
+		if (0 == maxlen)
+			return inputStr.substr(0, 1);
+		else
+			return result;
+	}
+};
--- MuyLeetCodeSolu/MuyLeetCodeSolu.cpp
+++ MuyLeetCodeSolu/MuyLeetCodeSolu.cpp
@@ -1,17 +1,13 @@
 ﻿// MuyLeetCodeSolu.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <algorithm>
-#include <functional>
+
 #include <iostream>
-#include <stack>
-#include <set>
-#include <map>
-#include <vector>
-#include <unordered_set>
+#include <string>
 
 // test
-#include "Easy/Problem204.h"
+
+#include "Medium/Problem5.h"
 
 //////// temp
 
@@ -21,8 +17,7 @@ int main()
 	std::cout << "Hello World!\n";
 
 
-	int result = 0;
-	result = Problem204::countPrimes(9);
+	std::string result = Problem5::longestPalindrome("babad");
 
 	std::cout << result << std::endl;
 	std::cout << "End\n";
