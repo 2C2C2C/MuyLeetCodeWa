5cc7bc760ce3c03e6632b4b5fd90283e474cdd54
HikoRyu <a2c2c123@126.com>
2025-03-09 16:49:46 +0800

    update Problem4

--- MuyLeetCodeSolu/Hard/Problem4.h
+++ MuyLeetCodeSolu/Hard/Problem4.h
@@ -3,6 +3,10 @@
 /*
 4. Median of Two Sorted Arrays
 https://leetcode.com/problems/median-of-two-sorted-arrays/submissions/
+
+Gather all num count.
+Calculate mid num index (count / 2), mid num will be 1 or 2.
+Do insert sorting until we meet the mis num index.
 */
 
 #include <vector>
@@ -12,40 +16,42 @@ class Problem4
 public:
 	double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2)
 	{
-		std::vector<int> tmpWa;
-		bool isOneMedian = true, finish = false;
-		int pos = 0, n1 = 0, n2 = 0, i = 0;
-		if (nums1.size())
+		std::vector<int> mergeArray;
+		bool isOneMedian = true;
+		int pos = 0, nums1Length = nums1.size(), nums2Lenth = nums2.size();
+		if (0!= nums1Length) // if nums1 is not empty
 			pos += nums1.size();
-		if (nums2.size())
+		if (0!= nums2Lenth) // if nums2 is not empty
 			pos += nums2.size();
 		if (pos % 2 == 0) // 
 			isOneMedian = false;
 		pos /= 2;
 
-		if (nums1.size() == 0)
+		bool  mergeFinished = false;
+		if (nums1Length == 0) // if nums1 is empty
 		{
-			tmpWa.insert(tmpWa.end(), nums2.begin(), nums2.end());
-			finish = true;
+			mergeArray.insert(mergeArray.end(), nums2.begin(), nums2.end());
+			mergeFinished = true;
 		}
-		else if (nums2.size() == 0)
+		else if (nums2Lenth == 0) // if nums2 is empty
 		{
-			tmpWa.insert(tmpWa.end(), nums1.begin(), nums1.end());
-			finish = true;
+			mergeArray.insert(mergeArray.end(), nums1.begin(), nums1.end());
+			mergeFinished = true;
 		}
 
-		// combine 2 array into 1 array
-		while (i <= pos && !finish)
+		int n1Index = 0, n2Index = 0, i = 0;
+		// merge 2 array into 1 array
+		while (i <= pos && !mergeFinished) // i <= pos cuz need to merge extra 1 element for calculation when there are 2 mid nums
 		{
-			if (n2 >= nums2.size() || (n1 < nums1.size() && nums1[n1] <= nums2[n2]))
-				tmpWa.push_back(nums1[n1++]);
+			if (n2Index >= nums2Lenth || (n1Index < nums1Length && nums1[n1Index] <= nums2[n2Index]))
+				mergeArray.push_back(nums1[n1Index++]);
 			else
-				tmpWa.push_back(nums2[n2++]);
+				mergeArray.push_back(nums2[n2Index++]);
 			i++;
 		}
 		if (isOneMedian) // get median
-			return tmpWa[pos];
+			return mergeArray[pos];
 		else
-			return ((double)(tmpWa[pos] + tmpWa[pos - 1])) / 2;
+			return ((double)(mergeArray[pos] + mergeArray[pos - 1])) / 2;
 	}
 };
